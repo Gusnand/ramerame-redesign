fe5d988ef6181bbb327a4dd917925f389b6c6e63
Gusnand <gusnanda1170@gmail.com>
2025-04-15 21:36:05 +0800

    adding sidebar and topbar just yet

--- resources/js/Pages/Super/Index.tsx
+++ resources/js/Pages/Super/Index.tsx
@@ -1,30 +1,32 @@
-import NavLink from "@/Components/NavLink";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Link, router } from "@inertiajs/react";
 
 export default function Index() {
-    const handleLogout = () => {
-        router.post("/logout");
-    };
+  const handleLogout = () => {
+    router.post("/logout");
+  };
 
-    const toProject = () => {
-        router.get("/super/project");
-    };
+  const toProject = () => {
+    router.get("/super/project");
+  };
 
-    return (
-        <div className="">
-            <h1 className="">Halo Super Admin!</h1>
-            <button
-                onClick={toProject}
-                className="bg-green-500 text-white px-4 py-2 rounded"
-            >
-                ke Halaman Project
-            </button>
-            <button
-                onClick={handleLogout}
-                className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
-            >
-                Logout
-            </button>
-        </div>
-    );
+  return (
+    <AuthenticatedLayout>
+      <div className="">
+        <h1 className="">Halo Super Admin!</h1>
+        <button
+          onClick={toProject}
+          className="bg-green-500 text-white px-4 py-2 rounded"
+        >
+          ke Halaman Project
+        </button>
+        <button
+          onClick={handleLogout}
+          className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+        >
+          Logout
+        </button>
+      </div>
+    </AuthenticatedLayout>
+  );
 }
